4443595799995aa2cf3cae2b29049a3c8dc4f845
zfzfg <163775060+zfzfg@users.noreply.github.com>
2025-11-05 12:36:04 +0100

    Delete src/main directory

--- src/main/java/de/zfzfg/pvpwager/models/MatchState.java
+++
@@ -1,8 +0,0 @@
-package de.zfzfg.pvpwager.models;
-
-public enum MatchState {
-    SETUP,       // Match is being set up (wager, arena, equipment selection)
-    STARTING,    // Countdown is running before the match starts
-    FIGHTING,    // Match is active, players are fighting
-    ENDED        // Match has ended (winner determined or draw)
-}
